f05faed65dc43de3e1d9212e02f6fea01db182bf
Blgt11147 <131634112+Blgt11147@users.noreply.github.com>
2024-01-22 21:38:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://singhbaljeet1299:6398gill@movierequest.hz1geaz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "movierequest")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://blgtsingh1299:EnNOfYnwzqtRCBCO@movie.dwjp2a7.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "movie")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
